0ec5cf4b992cff740febb07f0636c9a3e2d69882
Txm-dst <tom.dussautois@gmail.com>
2026-03-02 13:35:45 +0100

    Reprise TP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 import { getAllPokemons, getPokemonById, createPokemon, updatePokemon, deletePokemon } from './controllers/pokemon.controller';
 
 export const app = express();
 const port = process.env.PORT || 3000;
 
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/pokemons-cards', getAllPokemons);
 app.get('/pokemons-cards/:pokemonCardId', getPokemonById);
