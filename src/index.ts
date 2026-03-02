5a056ec7ef56c39a9d08466f599104654aee9baf
Txm-dst <tom.dussautois@gmail.com>
2026-03-02 16:57:19 +0100

    mise à jour de l'index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import YAML from 'yamljs';
 import path from 'path';
 import { getAllPokemons, getPokemonById, createPokemon, updatePokemon, deletePokemon } from './controllers/pokemon.controller';
 
+import { register, login } from './controllers/user.controller';
+import { authMiddleware } from './middlewares/auth.middleware';
+
 export const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,11 +16,16 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.post('/users', register);
+app.post('/users/login', login);
+
 app.get('/pokemons-cards', getAllPokemons);
 app.get('/pokemons-cards/:pokemonCardId', getPokemonById);
-app.post('/pokemons-cards', createPokemon);
-app.patch('/pokemons-cards/:pokemonCardId', updatePokemon);
-app.delete('/pokemons-cards/:pokemonCardId', deletePokemon);
+
+
+app.post('/pokemons-cards', authMiddleware, createPokemon);
+app.patch('/pokemons-cards/:pokemonCardId', authMiddleware, updatePokemon);
+app.delete('/pokemons-cards/:pokemonCardId', authMiddleware, deletePokemon);
 
 export const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
