083363a026ed10b13170a59731c09b62c3018c49
Txm-dst <tom.dussautois@gmail.com>
2026-03-19 13:29:02 +0100

    maj index.ts qui ne fonctionnait plus

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,21 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
 import { getAllPokemons, getPokemonById, createPokemon, updatePokemon, deletePokemon } from './controllers/pokemon.controller';
-
 import { register, login } from './controllers/user.controller';
 import { authMiddleware } from './middlewares/auth.middleware';
 
 export const app = express();
 const port = process.env.PORT || 3000;
 
-const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
-
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+try {
+  const swaggerPath = path.join(__dirname, '../swagger.yaml');
+  const swaggerDocument = YAML.load(swaggerPath);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (error) {
+  console.error("❌ Erreur de chargement du fichier swagger.yaml. Vérifie qu'il est à la racine du projet.");
+}
 
 app.post('/users', register);
 app.post('/users/login', login);
@@ -22,13 +25,15 @@ app.post('/users/login', login);
 app.get('/pokemons-cards', getAllPokemons);
 app.get('/pokemons-cards/:pokemonCardId', getPokemonById);
 
-
 app.post('/pokemons-cards', authMiddleware, createPokemon);
 app.patch('/pokemons-cards/:pokemonCardId', authMiddleware, updatePokemon);
 app.delete('/pokemons-cards/:pokemonCardId', authMiddleware, deletePokemon);
 
 export const server = app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+  console.log(`Serveur démarré`);
+  console.log(`URL : http://localhost:${port}`);
+
 });
 
 export function stopServer() {
